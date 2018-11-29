161dd2ecd5a3a09f682a688de408b8f109623da9
unknown <namoyang1205@naver.com>
2018-11-30 00:02:39 +0900

    Hello Git!

--- src/개인프로젝트/calculator.java
+++ src/개인프로젝트/calculator.java
@@ -9,19 +9,44 @@ import javax.swing.*;
 import java.awt.event.*;
 //[출처] [JAVA] Calendar (캘린더) 클래스 (추가: 날짜 계산 및 관련 함수, 날짜포맷;SimpleDateFormat)|작성자 자바킹
 public class calculator extends JFrame{
-
+	JLabel jl1 = new JLabel("연도");
+	JTextField tf1 = new JTextField(4);
+	JLabel jl2 = new JLabel("월");
+	JTextField tf2 = new JTextField(2);
+	JLabel jl3 = new JLabel("일");
+	JTextField tf3 = new JTextField(2);
+	JButton btn = new JButton("계산");
+	JLabel jl4 = new JLabel("D-");
+	JTextField tf4 = new JTextField(2);
+	JLabel jl5 = new JLabel("D+");
+	JTextField tf5 = new JTextField(2);
 	calculator(){
 		setTitle("Date Calculator");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container cp = getContentPane();
-		cp.setLayout(new FlowLayout());
-		JLabel jl1 = new JLabel("연도");
-		JTextField tf1 = new JTextField(4);
-		JLabel jl2 = new JLabel("월");
-		JTextField tf2 = new JTextField(2);
-		JLabel jl3 = new JLabel("일");
-		JTextField tf3 = new JTextField(2);
-		JButton btn = new JButton("계산");
+		cp.setLayout(null);
+		jl1.setLocation(320, 50);
+		tf1.setLocation(370, 50);
+		jl2.setLocation(320, 100);
+		tf2.setLocation(370, 100);
+		jl3.setLocation(320, 150);
+		tf3.setLocation(370, 150);
+		btn.setLocation(350, 200);
+		jl4.setLocation(350,250);
+		tf4.setLocation(370,250);
+		jl5.setLocation(350,300);
+		tf5.setLocation(370,300);
+		jl1.setSize(30,30);
+		tf1.setSize(80,30);
+		jl2.setSize(30,30);
+		tf2.setSize(80,30);
+		jl3.setSize(30,30);
+		tf3.setSize(80,30);
+		btn.setSize(60,30);
+		jl4.setSize(30,30);
+		tf4.setSize(60,30);
+		jl5.setSize(30,30);
+		tf5.setSize(60,30);
 		cp.add(jl1);
 		cp.add(tf1);
 		cp.add(jl2);
@@ -29,30 +54,60 @@ public class calculator extends JFrame{
 		cp.add(jl3);
 		cp.add(tf3);
 		cp.add(btn);
+		cp.add(jl4);
+		cp.add(tf4);
+		cp.add(jl5);
+		cp.add(tf5);
 		btn.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
+				int mesa = 0;
 				String year = tf1.getText();
-				for(int i=0;i<year.length();i++){
-					char c = year.charAt(i);
-					if(c<48 || c> 57){//숫자가 아닌 경우
-						break;
-					}
+				String month = tf2.getText();
+				String date = tf3.getText();
+				int b = Integer.parseInt(year);
+				int c = Integer.parseInt(month);
+				int d = Integer.parseInt(date);
+				switch(c) {
+				case 1:case 3:case 5:case 7:case 8:case 10:case 12:
+					if(b == 0|c<1 || c>12 | d<1 | d>31 & mesa ==0){
+						JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
+						mesa++; 
+					}break;
+				case 2:	if(b == 0|c<1 || c>12 | d<1 | d>28 & mesa ==0){
+					JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
+					mesa++; 
+				}break;
+				case 4:case 6:case 9:case 11:if(b == 0|c<1 || c>12 | d<1 | d>30 & mesa ==0){
+					JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
+					mesa++; 
+				}break;
+				default:
+					JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
+					mesa++; break;
 				}
-				//출처 : http://appsnuri.tistory.com/80 [이야기앱 세상]
+				if(mesa == 0) {
+					int kg = calculate(b,c,d);
+					String kfc = Integer.toString(kg);
+					if(kg>=0)
+					tf4.setText(kfc);
+					else
+						tf5.setText(kfc);
+				}
 			}
 		});
 		setSize(800,600);
 		setVisible(true);
 	}
 
-	void calculate (int a, int b, int c){
+	int calculate (int a, int b, int c){
 		Calendar cal = Calendar.getInstance();
 		//[출처] [JAVA] Calendar (캘린더) 클래스 (추가: 날짜 계산 및 관련 함수, 날짜포맷;SimpleDateFormat)|작성자 자바킹
 		int yr = cal.get(Calendar.YEAR);
-		int yr1 = yr;
-		int mn1 = cal.get(Calendar.MONTH);
-		int mn2 = mn1;
+		int mn = cal.get(Calendar.MONTH)+1;
 		int dt = cal.get(Calendar.DATE);
+		System.out.println(yr);
+		System.out.println(mn);
+		System.out.println(dt);
 		int md = 0;
 		int d[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		if(year(a)){ 
@@ -60,22 +115,8 @@ public class calculator extends JFrame{
 		}
 		else{
 			d[1] = 28;
-		};
-		for(;yr<= a; yr++){
-			if(mn1 == mn2 & yr == yr1){
-				md = d[mn1-1] - dt; 
-				mn1++;
-				for(; mn1 <= 12; mn1++){
-					md += d[mn1-1];   
-				}
-			}
-			else{
-				for(int i = 0; i < 12 ; i++){
-					if(yr == a & mn1 == b) { md += c; break;}
-					else md += d[i];
-				}
-			}
 		}
+		return md;
 	}
 	boolean year(int x){
 		if(x%4 == 0){
