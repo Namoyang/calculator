98fe99bf15e3899c7238e3d3b112df036b1cb372
unknown <namoyang1205@naver.com>
2018-12-02 00:14:53 +0900

    Hello Git!

--- src/개인프로젝트/calculator.java
+++ src/개인프로젝트/calculator.java
@@ -18,8 +18,8 @@ public class calculator extends JFrame{
 	JTextField tf3 = new JTextField(2);
 	JButton btn = new JButton("계산");
 	JTextField tf4 = new JTextField(2);
-	JLabel jl5 = new JLabel("요일");
 	JTextField tf5 = new JTextField(2);
+	JLabel jl4 = new JLabel("요일");
 	calculator(){
 		setTitle("Date Calculator");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -35,8 +35,8 @@ public class calculator extends JFrame{
 		tf3.setLocation(470,250);
 		btn.setLocation(450,300);
 		tf4.setLocation(470,350);
-		jl5.setLocation(430,400);
 		tf5.setLocation(470,400);
+		jl4.setLocation(505,400);
 		jl1.setSize(30,30);
 		tf1.setSize(80,30);
 		jl2.setSize(30,30);
@@ -45,8 +45,8 @@ public class calculator extends JFrame{
 		tf3.setSize(80,30);
 		btn.setSize(60,30);
 		tf4.setSize(60,30);
-		jl5.setSize(30,30);
-		tf5.setSize(60,30);
+		tf5.setSize(30,30);
+		jl4.setSize(30,30);
 		add(jl1);
 		add(tf1);
 		add(jl2);
@@ -55,8 +55,8 @@ public class calculator extends JFrame{
 		add(tf3);
 		add(btn);
 		add(tf4);
-		add(jl5);
 		add(tf5);
+		add(jl4);
 		btn.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				int mesa = 0;
@@ -68,54 +68,81 @@ public class calculator extends JFrame{
 				b = Integer.parseInt(year);
 				} catch(NumberFormatException nfe) {
 					b=0;
-					JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
 				} 
 				try {
 					c = Integer.parseInt(month);
 					} catch(NumberFormatException nfe) {
 						c=0;
-						JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
 					} 
 				try {
 					d = Integer.parseInt(date);
 				} catch(NumberFormatException nfe) {
 						d=0;
-						JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
 				} 
 				switch(c) {
 				case 1:case 3:case 5:case 7:case 8:case 10:case 12:
-					if(b == 0|c<1 || c>12 | d<1 | d>31 & mesa ==0){
+					if(b == 0|c<1 | c>12 | d<1 | d>31 & mesa ==0){
 						JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
 						mesa++; 
 					}break;
-				case 2:	if(b == 0|c<1 || c>12 | d<1 | d>28 & mesa ==0){
+				case 2:	if(b == 0|c<1 | c>12 | d<1 | d>28 & mesa ==0 & !year(b)){
+					JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
+					mesa++; 
+				}
+				else if(b == 0|c<1 | c>12 | d<1 | d>29 & mesa ==0 & year(b)) {
+					JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
+					mesa++;
+				}break;
+				case 4:case 6:case 9:case 11:if(b == 0|c<1 | c>12 | d<1 | d>30 & mesa ==0){
 					JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
 					mesa++; 
 				}break;
-				case 4:case 6:case 9:case 11:if(b == 0|c<1 || c>12 | d<1 | d>30 & mesa ==0){
-					JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
-					mesa++; 
-				}break;
-				default:
-					JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
-					mesa++; break;
+				default:JOptionPane.showMessageDialog(null, "다시 입력해주세요", "Message", JOptionPane.ERROR_MESSAGE);
+					    mesa++; 
+					    break;
 				}
 				if(mesa == 0) {
 					int kg = calculate(b,c,d);
 					String kfc = Integer.toString(kg);
-					if(kg>=0)
+					if(kg>=0) {
 					tf4.setText("D-" + kfc);
-					else {
-						kg = -kg;
-						tf5.setText("D+" + Integer.toString(kg));
+					tf5.setText(day(kg));
 					}
+					else {
+						int kj = -kg;
+						tf4.setText("D+" + Integer.toString(kj));
+						tf5.setText(day(kg));
+					}
+			       
 				}
 			}
 		});
 		setSize(1000,563);
 		setVisible(true);
 	}
-
+    String day(int a) {
+    	//
+    	Calendar cal= Calendar.getInstance ( );
+    	int day_of_week = cal.get ( Calendar.DAY_OF_WEEK );
+    	//[출처] [JAVA] Calendar (캘린더) 클래스 (추가: 날짜 계산 및 관련 함수, 날짜포맷;SimpleDateFormat)|작성자 자바킹
+    	int cv = a % 7;
+    	String m_week = null;
+    	if(a>0) {
+    	   cv += day_of_week;
+    	   if(cv>7) cv = cv % 7;
+    	   switch(cv) {
+    	   case 1: m_week = "일"; break;
+    	   case 2: m_week = "월"; break;
+    	   case 3: m_week = "화"; break;
+    	   case 4: m_week = "수"; break;
+    	   case 5: m_week = "목"; break;
+    	   case 6: m_week = "금"; break;
+    	   case 7: m_week = "토"; break;
+    	   }
+    	}
+        return m_week;
+    }
+	
 	int calculate (int a, int b, int c){
 		//
 		Calendar cal = Calendar.getInstance();
@@ -128,7 +155,31 @@ public class calculator extends JFrame{
 		int d1 = dt;
 		int md = 0;
 		int d[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		
+		if(a > yr) {
+			for(y1=yr;y1<=a;y1++) {
+				if(year(y1)) d[1] = 29;
+				else d[1] = 28;
+				if(y1==yr) {
+					for(int i = mn-1; i<12; i++) {
+						if(i==mn-1) md = d[i] - dt;
+						else md+=d[i];
+					}
+				}
+				else {
+					if(y1==a) {
+						for(int i =0; i<b; i++) {
+							if(i==b-1) md += c;
+							else md+=d[i];
+						}
+					}
+					else {
+						for(int i =0;i<12;i++) {
+							md += d[i];
+						}
+					}
+				}
+			}
+		}
 		return md;
 	}
 	boolean year(int x){
